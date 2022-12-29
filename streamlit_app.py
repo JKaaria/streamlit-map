cd1181e8eaa85f5eab0faaa082f19795a518af10
JKaaria <jeremykaaria994@gmail.com>
2022-12-29 16:07:00 +0000

    “update”

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,9 +8,9 @@ st.set_page_config(page_title="Streamlit Geospatial", layout="wide")
 # More icons can be found here: https://icons.getbootstrap.com
 
 apps = [
-    {"func": home.app, "title": "Home", "icon": "house"},
-    {"func": heatmap.app, "title": "Heatmap", "icon": "map"},
-    {"func": upload.app, "title": "Upload", "icon": "cloud-upload"},
+    {"func": home.app, "title": "", "icon": "house"},
+    {"func": heatmap.app, "title": "", "icon": "map"},
+    {"func": upload.app, "title": "", "icon": "cloud-upload"},
 ]
 
 titles = [app["title"] for app in apps]
@@ -31,18 +31,6 @@ with st.sidebar:
         icons=icons,
         menu_icon="cast",
         default_index=default_index,
-    )
-
-    st.sidebar.title("About")
-    st.sidebar.info(
-        """
-        This web [app](https://share.streamlit.io/giswqs/streamlit-template) is maintained by [Qiusheng Wu](https://wetlands.io). You can follow me on social media:
-            [GitHub](https://github.com/giswqs) | [Twitter](https://twitter.com/giswqs) | [YouTube](https://www.youtube.com/c/QiushengWu) | [LinkedIn](https://www.linkedin.com/in/qiushengwu).
-        
-        Source code: <https://github.com/giswqs/streamlit-template>
-
-        More menu icons: <https://icons.getbootstrap.com>
-    """
     )
 
 for app in apps:
